66c9cea74b42c507e34a39aacecbe598ae305986
Nicole Siomos <nicole.siomos@yahoo.com>
2020-12-14 21:17:01 +0100

    - Added logging support. Signed-off-by:Nicole Siomos <nicole.siomos@yahoo.com>

--- src/main/java/com/nicolesiomos/infinitedndserver/api/HealthController.java
+++ src/main/java/com/nicolesiomos/infinitedndserver/api/HealthController.java
@@ -1,6 +1,8 @@
 package com.nicolesiomos.infinitedndserver.api;
 
 import java.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HealthController {
+    private Logger logger = LoggerFactory.getLogger(HealthController.class);
+
+    
 	@RequestMapping(method = GET, path = "/health")
 	public HealthDto generateHealthReport() {
-            return new HealthDtoBuilder()
+            logger.info(String.format("Requested health report. Generating."));
+            
+            HealthDto ret = new HealthDtoBuilder()
                     .setState(HealthDto.State.OK)
                     .setTimestamp(LocalDateTime.now())
                     .createHealthDto();
+            
+            logger.info(String.format("Generated health report: \"%s\".", ret));
+            return ret;
 	}    
 }
